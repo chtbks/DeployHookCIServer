f27109a94cf2bc36ffd426abdf423ccf6bfcbf3a
Bert Curtis <bert.curtis22@gmail.com>
2018-09-28 13:45:10 -0600

    adding post to slack when post is recieved

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const PORT = process.env.PORT;
 
-app.get('/', (req, res) => {
-	res.send({ req });
+var request = require('request');
+function updateClient(postData) {
+	var clientServerOptions = {
+		uri:
+			'https://hooks.slack.com/services/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',
+		body: JSON.stringify(postData),
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	};
+	request(clientServerOptions, function(error, response) {
+		return;
+	});
+}
+
+app.post('/', function(req, res) {
+	if (req) {
+		res.send(updateClient(req.body));
+	}
 });
 
 app.listen(PORT);
