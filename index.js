518bba15b4e7d9db3e4af37fa62ce281229d8af8
Bert Curtis <bert.curtis22@gmail.com>
2018-10-01 12:12:02 -0600

    adding in routing for slack

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const shell = require('shelljs');
+const request = require('request');
 //const router = express.Router();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT;
 
-/*var request = require('request');
-function updateClient(postData) {
-	var clientServerOptions = {
+function postToMabl() {
+	shell.exec('./mabl.sh');
+}
+function postToSlack(payload) {
+	var bodyString = JSON.stringify(payload).replace(/\{|\}|\"/g, ' ');
+	var requestShiz = {
 		uri:
-			'https://hooks.slack.com/services/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',
-		body: JSON.stringify(postData),
+			'https://hooks.slack.com/services/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',
+		body: '{"text": "' + bodyString + '"}',
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json'
 		}
 	};
-	request(clientServerOptions, function(error, response) {
-		console.log(response.body);
+	request(requestShiz, function(error, response) {
+		console.log(error, response.body);
 		return;
 	});
-}*/
+	return bodyString;
+}
 //app.use('/api', router);
 
 //const originWhitelist = ['https://deployhookciserver.herokuapp.com'];
@@ -49,8 +55,22 @@ app.get('/', function(req, res) {
 });
 app.post('/', function(req, res) {
 	if (req.body) {
-		res.send(req.body);
+		try {
+			//postToMabl();
+			postToSlack(req.body);
+			res.send({
+				status: 200,
+				result: 'Success'
+			});
+		} catch (error) {
+			postToSlack('{"message": "' + error + '"}');
+			res.send({
+				status: 500,
+				result: 'Failure',
+				message: error + '. Could not execute POST requests to mabl and slack.'
+			});
+		}
 	}
 });
 
-app.listen(PORT);
+app.listen(3000);
